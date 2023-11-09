b89ff908742ae2597ce771db6999013fc78921fd
unknown <j3415879@naver.com>
2023-11-09 21:42:24 +0900

    ll

--- src/main/java/com/cos/blog/api/ControllerTest.java
+++ src/main/java/com/cos/blog/api/ControllerTest.java
@@ -1,12 +1,8 @@
 package com.cos.blog.api;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Controller
 public class ControllerTest {
